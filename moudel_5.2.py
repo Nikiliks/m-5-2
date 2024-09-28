ae1f8f2e1ce0980b256479c870b22830d19607e2
Nikiliks <nktalpeev@gmail.com>
2024-09-28 14:17:34 +0500

    moudel_5.2.py

---
+++ moudel_5.2.py
@@ -0,0 +1,22 @@
+# Задача "Developer - не только разработчик"
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        home = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return home
+
+
+h1 = House('Карточный домик', 10)
+h2 = House('Домик со стрелами', 20)
+
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
